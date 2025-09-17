e5eb8e3425e3251b5aa6bf57ffd41f316320ba5f
Om Rajput <rajputom2408@gmail.com>
2025-09-17 10:49:43 +0530

    Search Engine using Tools and Agents

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+"""
+LangChain Chat with Search (Streamlit App)
+------------------------------------------
+A chatbot that uses Groq LLM + LangChain Agents 
+with integrated Wikipedia, Arxiv, and DuckDuckGo search tools.
+"""
+
 import streamlit as st
 from langchain_groq import ChatGroq
 from langchain_community.utilities import WikipediaAPIWrapper, ArxivAPIWrapper
@@ -24,21 +31,36 @@ st.title("LangChain - Chat with Search")
 
 groq_api_key = os.getenv("GROQ_API_KEY")
 
-
+## Creating session history
 if "messages" not in st.session_state:
     st.session_state['messages'] = [
         {"role":"assistant", "content":"Hi! I'm a chatbot who can search the web. How can I help you?"}
     ]
 
+### For printing msgs in the UI
 for msg in st.session_state.messages:
     st.chat_message(msg['role']).write(msg['content'])
 
+### If prompt is added
+
 if prompt:=st.chat_input(placeholder="What is machine Learning?"):
+
+    ### Taking input from the user and printing it
     st.session_state.messages.append({"role":"user", "content":prompt})
     st.chat_message("user").write(prompt)
-    llm=ChatGroq(groq_api_key=groq_api_key,model_name="llama-3.1-8b-instant", streaming = True)
 
+    ### Initializing LLM
+
+    llm=ChatGroq(
+        groq_api_key=groq_api_key,
+        model_name="llama-3.1-8b-instant", 
+        streaming = True
+    )
+
+    ### Tools for AI agent
     tools = [search, arxiv_tool, wiki_tool]
+
+    ## Initializing Agent
 
     search_agent = initialize_agent(
         tools=tools, 
@@ -47,6 +69,7 @@ if prompt:=st.chat_input(placeholder="What is machine Learning?"):
         handling_parsing_errors = True
     )
 
+    ### assistant’s answer
     with st.chat_message("assistant"):
         st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
         response = search_agent.run(st.session_state.messages, callbacks=[st_cb])
